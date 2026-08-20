532c7b4aaa507e675f65754762ae5ac264576032
Sipendra Chaudhary <your-github-email@example.comsipendrachaudhary1@gmail.com>
2026-08-20 23:07:03 +0545

    sadcn-setup

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import Navbar from '../components/common/Navbar'
+import { Button } from '../components/ui/button'
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/hover-card"
+
 
 const Contact = () => {
   return (
     <div>
         <Navbar/>
+        <Button>click me</Button>
+        <Button variant='Rounded'> click me</Button>
+        <HoverCard>
+  <HoverCardTrigger>Hover</HoverCardTrigger>
+  <HoverCardContent>
+    Welcome to the contact pages
+  </HoverCardContent>
+</HoverCard>
+        
     </div>
   )
 }
